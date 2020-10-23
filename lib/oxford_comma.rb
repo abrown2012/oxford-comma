01cff6bcb162e5e1147e61e3bb1f4efd85e3d701
Agnieszka Brown <abrown2012@icloud.com>
2020-10-22 20:51:47 -0400

    commit tree c0eb19bf8fb6ad4d223f647a1de7b43bf338ed82

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.count == 1
+        return array[0]
+    elsif array.count == 2
+        return array.join(" and ")
+    else  
+        last = 'and ' + (array[-1])
+        array[-1] = last 
+        return array.join(", ")
+    end 
 end
